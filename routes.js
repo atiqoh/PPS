db8e69a37572da903da2971d10967558f0fe2638
arrijalifal <ifalgantengifal@gmail.com>
2026-06-03 13:19:32 +0700

    add /penggajian routes

--- routes.js
+++ routes.js
@@ -32,6 +32,7 @@ module.exports = (app) => {
   app.use("/auth", require("./routing/auth.route"));
   app.use("/pegawai", require("./routing/pegawai.routes"));
   app.use("/lembur", require("./routing/lembur.routes"));
+  app.use("/penggajian", require("./routing/penggajian.routes"));
 
   return app;
 
